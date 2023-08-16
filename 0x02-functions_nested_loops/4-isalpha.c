a25925117ec10a23c0b42453f4edcd856c16ce9f
sarahrefaat <sarahrefaatsss@gmail.com>
2023-08-16 00:25:43 -0700

    add the function description

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,7 @@
 #include "main.h"
-/*this is the function to check the c if it is a char or not*/
+
+/*_isalpha - checks for alphabetic character
+ * this is the function to check the c if it is a char or not*/
 int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
