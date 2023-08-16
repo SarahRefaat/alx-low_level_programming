4eb01037c40853b3257807e4469c29c891e57239
sarahrefaat <sarahrefaatsss@gmail.com>
2023-08-16 00:39:26 -0700

    add the function description

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+ * add - adds two int and returns their sum
+ * @a: int first number to add
+ * @b: int second number to be add to a
+ * Return: sum of a and b
+ */
 
 void print_alphabet(void)
 {
