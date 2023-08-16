ada30ab7116edfcd180a62e409ad020a3f9c2754
sarahrefaat <sarahrefaatsss@gmail.com>
2023-08-16 00:32:08 -0700

    trying a new commenting style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
-/*_isalpha - checks for alphabetic character
- * this is the function to check the c if it is a char or not*/
+/**
+ * _isalpha - checks for alphabetic character
+ * this is the function to check the c if it is a char or not
+ */
 int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
