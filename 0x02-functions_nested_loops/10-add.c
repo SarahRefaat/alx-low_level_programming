6193b96edeebfa0b016f502378e625e65460147b
sarahrefaat <sarahrefaatsss@gmail.com>
2023-08-16 17:31:12 -0700

    the easiest function, you cam ever get

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two int and returns their sum
+ * @a: int first operator for the addation operator
+ * @b: int second operator for the addation operator
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
